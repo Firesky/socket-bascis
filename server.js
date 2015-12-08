cd5037ecea9d1060399e6d5478da0c0bb6f14a61
Luke Clifford <luke@firesky.com.au>
2015-12-08 22:06:45 +1100

    Add disconnect event and message

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ var clientInfo = {};
 io.on('connection', function(socket){
 	console.log('User connected via socket.io');
 
+	socket.on('disconnect', function(){
+		var userData = clientInfo[socket.id];
+		if(typeof clientInfo[socket.id] !== 'undefined'){
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left the room!',
+				timestamp: moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function(req){
 		console.log(socket.id);
 		clientInfo[socket.id] = req;
